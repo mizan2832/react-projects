a0365039a00e20dd94b6abb27a7ddb405817b734
mizan2832 <=>
2022-01-25 22:28:19 +0600

    tour complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './birthday/App';
-import './birthday/index.css'
+import App from './tour/App';
+import './tour/index.css'
 
 
 ReactDOM.render(
--- src/tour/App.js
+++ src/tour/App.js
@@ -17,18 +17,40 @@ const fetchTours = async () => {
   setLoading(true)
   try{
     const response = await fetch(url)
+    
     const tours = await response.json()
+    // console.log(tours);
+
     setLoading(false)
     setTours(tours)
   }catch (error){
     setLoading(false)
-    console.log(error);
+    // console.log(error);
   }
 }
 
 useEffect(() => {
   fetchTours()
 }, [])
+
+if (loading) {
+  return(
+    <main>
+      <Loading />
+    </main>
+  )
+}
+
+if (tours.lengh === 0) {
+  return (
+    <main>
+      <div className="title">
+        <h2>no tours left</h2>
+        <button className="btn" onClick={() => fetchTours()}>refresh</button>
+      </div>
+    </main>
+  )
+}
 
 
     
